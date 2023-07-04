8f173e972d08a388b8e37194d6bb8f1ac00fccf5
Daniel Orfeu <daniel_yago1997@hotmail.com>
2023-07-03 23:03:46 -0300

    fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
 'use client'
+import { VStack } from '@chakra-ui/react'
 import React from 'react'
-import { styled } from 'styled-components'
 
-export const Container = styled.main`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  /* padding: 6rem; */
-  /* min-height: 100vh; */
-`
+
 
 export default function Home() {
   return (
-    <Container>
+    <VStack>
       <h1>Home</h1>
-    </Container>
+    </VStack>
   )
 }
